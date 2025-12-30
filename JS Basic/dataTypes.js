fd8a8ce5bc97a4b660dc02ff7f51f6f9e888a7b8
Haram Awan <awanharam70@gmail.com>
2025-12-30 08:18:38 -0800

    datatypes

---
+++ JS Basic/dataTypes.js
@@ -0,0 +1,41 @@
+// const { StrictMode } = require("react")
+
+// console.log("hello")
+let a =19
+var b=5.6
+const c="a"
+d=100
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// Symbol
+// BigInt
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Object
+let accountName="Hello"
+const accountId="123456"
+let accountPassword="hy123"
+// console.log(accountName);
+// console.log(accountId);
+// console.log(accountPassword);
+// console.table([accountName,accountId,accountPassword])
+// console.log(typeof accountPassword);
+// console.log(typeof accountId);
+// ECMA Script E6 2015
+// use StrictMode
+
+if(true){
+    var a1=10
+    let b1=20
+    b1=30
+    // console.log(a1);
+    // console.log(b1);
+    
+}
+    // console.log(a1);
+    console.log(b1);
